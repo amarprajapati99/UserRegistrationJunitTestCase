8730c7b1812b883984451c0aa5db9d77eecc3191
amarprajapati99 <amarprajapati99@gmail.com>
2021-04-02 00:49:47 +0530

    User need to enter valid last name

--- src/main/java/com/userregistration/bridgelabz/UserValidator.java
+++ src/main/java/com/userregistration/bridgelabz/UserValidator.java
@@ -4,8 +4,12 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/* @Description- user enter first name and last name using regex condition.
+* first latter must be start with cap letter.
+ */
 public class UserValidator {
     private String firstName;
+    private String lastName;
 
     public String getFirstName() {
         Scanner scanner = new Scanner(System.in);
@@ -20,7 +24,14 @@ public class UserValidator {
         boolean matches = matcher.find();
                 return matches;
     }
-
+    /* @Description User enter a valid last name.
+     */
+    public boolean validateLastName(String lastName) {
+        Pattern pattern = Pattern.compile("[A-Z]{1}[a-z]{2}+");
+        Matcher matcher = pattern.matcher(lastName);
+        boolean matches = matcher.find();
+        return matches;
+    }
     public static void main(String[] args) {
         UserValidator userValidator = new UserValidator();
         String firstName = userValidator.getFirstName();
--- src/test/java/com/userregistration/bridgelabz/UserValidatorTest.java
+++ src/test/java/com/userregistration/bridgelabz/UserValidatorTest.java
@@ -5,10 +5,17 @@ import org.junit.jupiter.api.Test;
 
 public class UserValidatorTest {
     UserValidator userValidator = new UserValidator();
-    @Test
-   public void givenFirst_whenProper_shouldReturnTrue() {
-        boolean result = userValidator.validateName("Jay");
-        Assert.assertEquals(result,true);
 
+    @Test
+    public void givenFirst_whenProper_shouldReturnTrue() {
+        boolean result = userValidator.validateLastName("Jay");
+        Assert.assertEquals(result, true);
+
+    }
+
+    @Test
+    public void givenLastName_whenProper_shouldReturnTrue() {
+        boolean result = userValidator.validateLastName("Praj");
+        Assert.assertEquals(result, true);
     }
 }
