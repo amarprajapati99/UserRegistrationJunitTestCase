e60971de961fb004579c7af0668e5d4eeb671d13
amarprajapati99 <amarprajapati99@gmail.com>
2021-04-08 12:23:54 +0530

    Pass parameter for multiple emails

---
+++ src/test/java/com/userregistration/bridgelabz/EmailValidationTest.java
@@ -0,0 +1,50 @@
+package com.userregistration.bridgelabz;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import java.util.Arrays;
+import java.util.Collection;
+@RunWith(Parameterized.class)
+    public class EmailValidationTest {
+    private String email;
+    private boolean expectedResult;
+    private UserValidator registerdEmail;
+    public EmailValidationTest(boolean expected, String email) {
+       super();
+       this.email = email;
+       this.expectedResult = expected;
+    }
+    @Before
+    public void initialize() {
+    registerdEmail = new UserValidator();
+    }
+ @Parameterized.Parameters
+        public static Collection input() {
+            return Arrays.asList(new Object[][]{
+               {true, "abc@yahoo.com"},
+               {true, "abc-100@yahoo.com"},
+               {true, "abc.100@yahoo.com"},
+               {true, "abc111@abc.com"},
+               {true, "abc-100@abc.net"},
+               {true, "abc.100@abc.com.au"},
+               {true, "abc@1.com"},
+               {true, "abc@gmail.com.com"},
+               {true, "abc+100@gmail.com"},
+               {false, "abcgmail.com"},
+               {false, "abc@.com.my"},
+               {false, "abc.100@gmail.a"},
+               {false, "abc123@.com"},
+               {false, "abc123@.com.com"},
+               {false, ".abc@abc.com"},
+            });
+  }
+  @Test
+      public void testEmailValidation() {
+          System.out.println("expected result for email is " + expectedResult);
+          Assert.assertEquals(expectedResult, registerdEmail.validateEmail(email));
+
+    }
+}
+
